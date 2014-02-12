3b2374acd7cdeb11b638dc0d0b227370cf735170
Kristján Pétursson <kristjan@gmail.com>
2014-02-12 00:45:05 -0800

    185

--- pig_latin.min.rb
+++ pig_latin.min.rb
@@ -1,9 +1,8 @@
-W='aeuio'
-V=/(?<!^)([#{W}y])/
-U=/[#{W}]/i
+V=/([aeiuoy])/
+U=/[aeiou]/i
 def(PigLatin=?a).translate(s)
 s.gsub(/\w+/){|w|
 w[0]=~U ? w+'way':
-(w=w.split(V,2).rotate.join+'ay'
+(w=(w.split(V,2)-['']).rotate.join+'ay'
 w=~/[A-Z]/?w[0].upcase+w[1..-1].downcase: w)}
 end
