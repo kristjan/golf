888ac7a10236316e2b321118e2e5911e433db055
Kristján Pétursson <kristjan@gmail.com>
2014-02-12 01:16:58 -0800

    167 chars

--- min.rb
+++ min.rb
@@ -1,8 +1,2 @@
-V=/([aeiuoy])/
-U=/[aeiou]/i
-def(PigLatin=?a).translate(s)
-s.gsub(/\w+/){|w|
-w[0]=~U ? w+'way':
-(w=(w.split(V,2)-['']).rotate.join+'ay'
-w=~/[A-Z]/?w[0].upcase+w[1..-1].downcase: w)}
-end
+def(PigLatin=?a).translate(s)s.gsub(/\w+/){|w|w[/^[aeiou]/i]?w+'way':(w=(w.split(/([aeiuoy])/)-['']).rotate.join+'ay'
+w=~/[A-Z]/?w[0].upcase+w[1..-1].downcase: w)}end
