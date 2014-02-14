ad1352189811dde859a1000ccd4fc92afab98378
Kristján Pétursson <kristjan@gmail.com>
2014-02-14 01:34:45 -0800

    Run latins in descending size

--- pig_latin/run.rb
+++ pig_latin/run.rb
@@ -1,10 +1,15 @@
 #!/usr/bin/env ruby
 
-latins = Dir['*.rb'].reject{|file| file =~ /pig_latin|run/}
-latins.each do |latin|
-  chars = `wc -c #{latin}`.to_i
-  puts "Running #{latin} with #{chars} chars"
+latins = Dir['*.rb']
+  .reject{|file| file =~ /pig_latin|run/}
+  .map{|latin| [latin, `wc -c #{latin}`.to_i]}
+  .sort_by(&:last)
+  .reverse
+
+latins.each do |latin, size|
+  puts "\nRunning #{latin} with #{size} chars"
   `ln -sf #{latin} pig_latin.rb`
   system('rspec pig_latin_spec.rb')
 end
+
 `rm pig_latin.rb`
