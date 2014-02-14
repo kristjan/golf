17e29cfd0bc763fb472d7eac57d605a907185e03
Kristján Pétursson <kristjan@gmail.com>
2014-02-14 01:23:36 -0800

    Clean readable version

--- readable.rb
+++ readable.rb
@@ -1,20 +1,16 @@
-# TODO: Load from web
-
-SHOWIT = ->(s) {puts s.inspect}
 START_VOWELS = /([aeiou])/i
 VOWELS = /(?<!^)([aeiouy])/i
 
 class PigLatin
   def self.translate(str)
-    str.split(/(\W+)/).map(&method(:word)).join
+    str.gsub(/(\w+)/, &method(:word))
   end
 
   def self.word(w)
-    return w unless w =~ /\w/
     return w + 'way' if w[0] =~ START_VOWELS
-    cap = w[0] =~ /[A-Z]/
-    w = w.downcase.split(VOWELS, 2).rotate.join + 'ay'
-    w = w[0].upcase + w[1..-1] if cap
+    i = w =~ VOWELS
+    w = w[i..-1] + w[0...i] + 'ay'
+    w = w[0].upcase + w[1..-1].downcase if w =~ /[A-Z]/
     w
   end
 end
